3a86dfef76009229190fcac904fed53d00a9d4db
Daniel Mabadeje <mabadejedaniel1@gmail.com>
2020-03-24 04:48:23 -0700

    update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,15 @@
 <?php
 //DB Params
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'notes');
+$url=  parse_url(postgres://kpodalbannhtcn:45d51c05b027cdd9fce5b614e744d6d1a7323a89d0c3162f5bacec5c94a13f34@ec2-34-197-212-240.compute-1.amazonaws.com:5432/d7hb6m2ef7tdct);
+
+define('DB_HOST', $url["host"]);
+define('DB_USER', $url["user"]);
+define('DB_PASS', $url["pass"]);
+define('DB_NAME', substr($url["path"], 1));
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'notes');
 //App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 define('URLROOT', 'https://acesnote.herokuapp.com/');
